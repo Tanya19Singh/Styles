c0b0e5f604dadfe01c25a5a39ff92dd8da32b18b
Tanya Singh <98661040+Tanya19Singh@users.noreply.github.com>
2025-02-13 04:51:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This allows binding to 0.0.0.0
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
